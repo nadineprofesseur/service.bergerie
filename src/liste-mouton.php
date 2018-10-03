1d9d4c06628211382665de49dcda0c1f003ea46f
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-03 14:53:19 -0400

    Quelques champs dans /mouton/liste/

--- src/liste-mouton.php
+++ src/liste-mouton.php
@@ -5,9 +5,20 @@
 	$requeteListeMoutons = $basededonnees->prepare($SQL_LISTE_MOUTONS);
 	$requeteListeMoutons->execute();
 	$listeMoutons = $requeteListeMoutons->fetchAll(PDO::FETCH_OBJ);
-	print_r($listeMoutons);
+	//print_r($listeMoutons);
 ?>
 <moutons><description>Liste des moutons</description>
 
+<?php
+	foreach($listeMoutons as $mouton)
+	{
+	?>
+	<mouton>
+		<nom><?=$mouton->nom?></nom>
+		<couleur><?=$mouton->couleur?></couleur>
+	</mouton>
+	<?php
+	}
+?>
 
 </moutons>
