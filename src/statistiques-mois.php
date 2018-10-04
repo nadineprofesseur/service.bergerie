5cbe701c7e1676bc46000342ca64406aacb7ab7d
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-04 11:14:24 -0400

    Format xml pour les statistiques d'un mois (ou autre)

--- src/statistiques-mois.php
+++ src/statistiques-mois.php
@@ -1,3 +1,30 @@
 <?php
-	print_r($_GET);
+	//print_r($_GET);
+	include "connection.php";
+	
 ?>
+<statistiques-mois>
+	<mois>10</mois>
+	<statistiques>
+	
+		<intensite>
+			<max></max>
+			<min></min>
+			<moyenne></moyenne>
+		</intensite>
+		
+		<duree>
+			<max></max>
+			<min></min>
+			<moyenne></moyenne>		
+		</duree>
+
+		<repetition>
+			<max></max>
+			<min></min>
+			<moyenne></moyenne>		
+		</repetition>
+		
+	</statistiques>
+
+</statistiques-mois>
