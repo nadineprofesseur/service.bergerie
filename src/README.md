367d048345985f347f5ae2fb7fa92943af7dab2b
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-04 10:54:27 -0400

    Test pour /mouton/1/

--- src/README.md
+++ src/README.md
@@ -1,1 +1,6 @@
 # service.bergerie
+
+URL fonctionnelles : 
+
+view-source:http://localhost/service.bergerie/mouton/liste/
+view-source:http://localhost/service.bergerie/mouton/1/
--- src/mouton.php
+++ src/mouton.php
@@ -1,7 +1,12 @@
 <?php 
 
-	include "connection.php";
-
 	print_r($_GET);
+	
+	$SQL_MOUTON = "SELECT * FROM mouton WHERE id = " . $_GET['mouton']; // TODO requete preparee
+	include "connection.php";
+	$requeteMouton = $basededonnees->prepare($SQL_MOUTON);
+	$requeteMouton->execute();
+	$mouton = $requeteMouton->fetch(PDO::FETCH_OBJ);
+	//print_r($mouton);
 ?>
 <mouton></mouton>
