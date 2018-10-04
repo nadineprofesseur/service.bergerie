96f3b051fdb60b5c1093bedbe984c2c5b7c5156a
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-04 10:56:01 -0400

    Service d'un xml de un mouton

--- src/mouton.php
+++ src/mouton.php
@@ -1,6 +1,8 @@
 <?php 
+	header("Content-type: text/xml");
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
 
-	print_r($_GET);
+	//print_r($_GET);
 	
 	$SQL_MOUTON = "SELECT * FROM mouton WHERE id = " . $_GET['mouton']; // TODO requete preparee
 	include "connection.php";
@@ -9,4 +11,11 @@
 	$mouton = $requeteMouton->fetch(PDO::FETCH_OBJ);
 	//print_r($mouton);
 ?>
-<mouton></mouton>
+<mouton>
+	<nom><?=$mouton->nom?></nom>
+	<couleur><?=$mouton->couleur?></couleur>
+	<poids><?=$mouton->poids?></poids>
+	<naissance><?=$mouton->naissance?></naissance>
+	<description><?=$mouton->description?></description>		
+
+</mouton>
