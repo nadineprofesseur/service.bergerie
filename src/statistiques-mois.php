deffb93e27ab5ea784c38c42c8be2eb3252c0308
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-12-21 15:00:51 -0500

    Premier jet de statistiques mois

--- src/statistiques-mois.php
+++ src/statistiques-mois.php
@@ -1,11 +1,23 @@
 <?php
-	//print_r($_GET);
+	//print_r($_GET); // TODO restreindre en fonctino du mois
 	include "connection.php";
-	
+	$SQL_STATISTIQUES_CLOCHETTE_MOIS = "SELECT max(intensite) as maxIntensite, min(intensite) as minIntensite, avg(intensite), max(duree) as maxDuree, min(duree) as minDuree, avg(duree) as moyenneDuree, max(repetition) as maxRepetition, min(repetition) as minRepetition, avg(repetition) as moyenneRepetition, count(repetition) as total FROM clochette";
+	//$SQL_STATISTIQUES_PAR_MOUTON = "SELECT max(repetition) as maxRepetition, min(repetition) as minRepetition, avg(repetition) as moyenneRepetition, count(repetition) as total FROM clochette GROUP by mouton";
 ?>
 <statistiques-mois>
 	<mois>10</mois>
 	<statistiques>
+	
+		<nombre> 
+			<total></total>
+			<par-mouton>
+				<total></total>
+				<max></max>
+				<min></min>
+				<moyenne></moyenne>			
+			</par-mouton>
+			
+		</nombre>
 	
 		<intensite>
 			<max></max>
